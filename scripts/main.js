12eb2da798da992e463847278928a07d1a289c06
tomoki-yamamura <progtab17@gmail.com>
2021-06-06 01:57:40 +0900

    20210605

--- scripts/main.js
+++ scripts/main.js
@@ -21,6 +21,57 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
+    // const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
+
+    new MobileMenu();
+    new Main();
 });
+
+class Main{
+    constructor(){
+        this.header = document.querySelector('.header');
+        this._observers = [];
+        this._init();
+    }
+
+    _textAnimation(el, inview) {
+        if(inview) {
+            const ta = new TweenTextAnimation(el);
+            ta.animate();
+        }
+    }
+
+    _init(){
+        const hero = new HeroSlider('.swiper-container');
+        this._scrollInit();
+    }
+
+    _scrollInit(){
+        this._observers.push(
+            new ScrollObserver('.nav-trigger', this._navAmination.bind(this), {once: false}),
+            new ScrollObserver('.cover-slide', this._inviewAnimation),
+            new ScrollObserver('.tween-animate-title', this._textAnimation),
+            new ScrollObserver('.swiper-container', _textAnimation.bind(this), )
+        );
+    };
+
+    _toggleSlideAnimation(el, inview) {
+        if(inview) {
+            this.hero.start();
+        } else {
+            this.hero.stop();
+        }
+    }
+
+    _navAmination(el, inview) {
+        if(inview) {
+            this.header.classList.remove('triggered');
+        } else {
+            this.header.classList.add('triggered');
+        }
+    }
+    
+}
+
+
 

d232bfe5a73cf5614e5a130c0b1f6f73bbe8477c
tomoki-yamamura <progtab17@gmail.com>
2021-06-08 01:59:17 +0900

    210608

--- scripts/libs/scroll.js
+++ scripts/libs/scroll.js
@@ -27,10 +27,7 @@ class ScrollObserver {
         };
 
         this.io = new IntersectionObserver(callback.bind(this), this.options);
-
-        // @see https://github.com/w3c/IntersectionObserver/tree/master/polyfill
         this.io.POLL_INTERVAL = 100;
-        
         this.els.forEach(el => this.io.observe(el));
     }
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,55 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const hero = new HeroSlider('.swiper-container');
-    hero.start();
+    const main = new Main();
+});
 
-
-    const cb = function (el, inview) {
-        if(inview) {
-            const ta = new TweenTextAnimation(el);
-            ta.animate();
-        }
+class Main {
+    constructor() {
+        this.header = document.querySelector('.header');
+        this.sides = document.querySelectorAll('.side');
+        this._observers = [];
+        this._init();
     }
 
-    const so = new ScrollObserver('.tween-animate-title', cb);
+    set observers(val) {
+        this._observers.push(val);
+    }
 
+    get observers() {
+        return this._observers;
+    }
 
-    const _inviewAnimation = function(el, inview) {
+    _init() {
+        new MobileMenu();
+        // this.hero = new HeroSlider('.swiper-container');
+        Pace.on('done', this._paceDone.bind(this));
+    }
+
+    _paceDone() {
+        this._scrollInit();
+    }
+
+    _inviewAnimation(el, inview) {
         if(inview) {
-            el.classList.add('inview'); 
-        } else {
+            el.classList.add('inview');
+        }else {
             el.classList.remove('inview');
         }
     }
 
-    // const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
+    _navAnimation(el, inview) {
+        if(inview) {
+            this.header.classList.remove('triggered');
+        } else {
+            this.header.classList.add('triggered');
+        }
+    }
 
-    new MobileMenu();
-    new Main();
-});
-
-class Main{
-    constructor(){
-        this.header = document.querySelector('.header');
-        this._observers = [];
-        this._init();
+    _sideAnimation(el, inview) {
+        if(inview) {
+            this.sides.forEach(side => side.classList.add('inview'));
+        } else {
+            this.sides.forEach(side => side.classList.remove('inview'));
+        }
     }
 
     _textAnimation(el, inview) {
@@ -41,20 +59,6 @@ class Main{
         }
     }
 
-    _init(){
-        const hero = new HeroSlider('.swiper-container');
-        this._scrollInit();
-    }
-
-    _scrollInit(){
-        this._observers.push(
-            new ScrollObserver('.nav-trigger', this._navAmination.bind(this), {once: false}),
-            new ScrollObserver('.cover-slide', this._inviewAnimation),
-            new ScrollObserver('.tween-animate-title', this._textAnimation),
-            new ScrollObserver('.swiper-container', _textAnimation.bind(this), )
-        );
-    };
-
     _toggleSlideAnimation(el, inview) {
         if(inview) {
             this.hero.start();
@@ -63,15 +67,24 @@ class Main{
         }
     }
 
-    _navAmination(el, inview) {
-        if(inview) {
-            this.header.classList.remove('triggered');
-        } else {
-            this.header.classList.add('triggered');
-        }
+    _destroyObservers() {
+        this.observers.forEach(ob => {
+            ob.destroy();
+        });
     }
-    
+
+    destroy() {
+        this._destroyObservers();
+    }
+
+    _scrollInit() {
+        this.observers = new ScrollObserver('.nav-trigger', this._navAnimation.bind(this), {once: false});
+        this.observers = new ScrollObserver('.cover-slide', this._inviewAnimation);
+        this.observers = new ScrollObserver('.appear', this._inviewAnimation);
+        this.observers = new ScrollObserver('.tween-animate-title', this._textAnimation, {rootMargin: "-200px 0px"});
+        this.observers = new ScrollObserver('.swiper-container', this._toggleSlideAnimation.bind(this), {once: false});
+        this.observers = new ScrollObserver('#main-content', this._sideAnimation.bind(this), {once: false, rootMargin: "-300px 0px"});
+        console.log(this.observers)
+    }
 }
-
-
 
